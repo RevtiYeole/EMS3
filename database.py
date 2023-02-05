cb51fe500589a0f774b2475655e02c7b9278e15f
RevtiYeole <124459483+RevtiYeole@users.noreply.github.com>
2023-02-05 21:41:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from flask import g
 import sqlite3 
 
 def connect_to_database():
-    sql = sqlite3.connect('C:/Users/REVTI/OneDrive/Desktop/EMS/crudapplication.db')
+    sql = sqlite3.connect('crudapplication.db')
     sql.row_factory = sqlite3.Row
     return sql 
 
